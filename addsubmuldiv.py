73aab9ad52e92a1f21b75426c803b6e04a35a7e3
Prajukta <prajuktaha@gmail.com>
2025-08-01 22:47:08 +0530

    addition substraction multiplication and divison of two numbers

---
+++ addsubmuldiv.py
@@ -0,0 +1,10 @@
+a = 2
+b = 5
+sum = a + b
+print("the sum of a and b is", sum)
+diff = a - b
+print("the difference of a and b is", diff)
+prod = a*b
+print("the product of a and b is", prod)
+quot = a/b
+print("the quotient of a and b is", quot)
