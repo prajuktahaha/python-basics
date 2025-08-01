a58ffbeb265de5d8f2c3b98948c2923be662140c
Prajukta <prajuktaha@gmail.com>
2025-08-01 22:16:58 +0530

    trials

--- firstprgm.py
+++ firstprgm.py
@@ -3,3 +3,7 @@ price = 20.00
 print("my name is : ", name)
 print("my age is : ", age)
 print("my price is : ", price)
+// to identify the datatypes of
+print(type(name))
+print(type(age))
+print(type(price))
