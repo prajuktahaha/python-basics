f0e9ebfde6aaf8302bf5e1fec52b629803d00cc7
Prajukta <prajuktaha@gmail.com>
2025-08-02 23:36:15 +0530

    input 2 numbers ad print their sum

---
+++ sumoftwonumbers.py
@@ -0,0 +1,5 @@
+#input 2 numbers ad print their sum
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+sum = int(num1) + int(num2)
+print("sum of two numbers is " + str(sum))
