2c9eec0b7f0cb118400347b48b05b84239ed6fd9
Prajukta <prajuktaha@gmail.com>
2025-08-03 00:06:44 +0530

    Average of two numbers

---
+++ averageof2number.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+sum = float(num1) + float(num2)
+print("sum of two numbers is" , str(sum))
+average = sum/2
+print("average of two number is" , str(average))
