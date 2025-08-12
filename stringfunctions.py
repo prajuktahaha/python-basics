cc904bf4abc9bee99ce689c5163b9406483dc5dd
Prajukta <prajuktaha@gmail.com>
2025-08-12 22:31:02 +0530

    types of string functions

---
+++ stringfunctions.py
@@ -0,0 +1,9 @@
+str = "i'm prajukta harichandan"
+print(str.endswith("harichandan"))
+print(str.endswith("pp"))
+print(str.capitalize())
+print(str.replace("prajukta" , "preeti"))
+print(str.replace("p" , "a"))
+print(str.find("prajukta"))
+print(str.find("z")) #this will give -1
+print(str.count("a"))
