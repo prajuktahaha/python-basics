82a843cbfc7ee2c7b16a0598ca815935a2e98fec
Prajukta <prajuktaha@gmail.com>
2025-08-25 20:55:44 +0530

    traffic light

---
+++ trafficlight.py
@@ -0,0 +1,9 @@
+light = input("Enter the color: ")
+if(light == "red"):
+    print("Stop")
+elif(light == "yellow"):
+    print("wait")
+elif(light == "green"):
+    print("go")
+else:
+    print("invalid color")            
