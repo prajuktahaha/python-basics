57ff6d48e0f51a7cb123fae2fe4c9428cca5e4f4
Prajukta <prajuktaha@gmail.com>
2025-08-27 00:14:44 +0530

    grading category

---
+++ grade.py
@@ -0,0 +1,20 @@
+marks=int(input("enter your mark :"))
+
+if(marks >90):
+  print("your grade is A1")
+elif(90>=marks>80):
+  print("Your grade is A2")
+elif(80 >=marks >70):
+  print("Your grade is B1")
+elif(70 >=marks > 60):
+  print("Your grade is B2")
+elif(60>= marks > 50):
+  print("Your grade is C1")
+elif(50>= marks > 40):
+  print("Your grade is C2")
+elif(40>= marks > 30):
+  print("Your grade is D")
+elif(30>= marks > 20):
+  print("Your grade is E")
+elif(20>= marks >=0):
+  print("Your grade is F")
