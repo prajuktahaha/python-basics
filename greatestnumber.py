b6512595342f49db37f26dbb79abf3e0617b6afe
Prajukta <prajuktaha@gmail.com>
2025-08-27 22:53:30 +0530

    find the greatest number

---
+++ greatestnumber.py
@@ -0,0 +1,9 @@
+num0 = int(input("Enter the numbers :"))
+num1 = int(input("enter the numbers :"))
+num2 = int(input("enter the numbers :"))
+if(num0>num1) and (num0>num2):
+    print(num0 , "is the greatest number")
+elif(num1>num0) and (num1>num2):
+    print(num1 , "is the geatest number")
+elif(num2>num0) and (num2>num1):
+    print(num2 , "is the greatest number")    
