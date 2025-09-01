e1d67c7182f129864d9d43b6bbd6fc442127d53c
Prajukta <prajuktaha@gmail.com>
2025-09-02 00:22:05 +0530

    store 3 movies name in listingmethod

---
+++ movienames.py
@@ -0,0 +1,8 @@
+movies = []
+mov1 = input("Enter the name of movie 1: ")
+mov2 = input("Enter the name of movie 2: ")
+mov3 = input("Enter the name of movie 3: ")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
