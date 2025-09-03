6ff7cca363fef21cbae77d8dc1828a01131ea0b9
Prajukta <prajuktaha@gmail.com>
2025-09-03 23:27:40 +0530

    dictionaries in python basic

--- abtme.py
+++ abtme.py
@@ -6,3 +6,6 @@ info = {
 }
 
 print(type(info)) 
+//if you want to print surname then u can simply delete the upper line code and enter this and thsi will print entire name and surname//
+info["surname"] = "Harichandan"
+print(info)
