8379b7f2cd054cac5fea368d064d3b05825990b1
Prajukta <prajuktaha@gmail.com>
2025-09-06 23:17:08 +0530

    setcollections

---
+++ setcollections.py
@@ -0,0 +1,5 @@
+collection = {1 , 2 ,3 ,4}
+#if i will repeat anything it will print only once
+print(collection)
+print(type (collection))
+print(len(collection)) #even in length it wont print duplicate one
