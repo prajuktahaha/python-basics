aacc0e71a2e017c24be853e72c6ceb6049f71ea3
Prajukta <prajuktaha@gmail.com>
2025-09-07 22:16:45 +0530

    store word meaning in a python

--- testcase1.py
+++ testcase1.py
@@ -1,5 +1,7 @@
+#Q1 store the foll word meaning in a python dictionary#
 dictionary = {
     "table" : "a piece of furniture" ,
     "cat" : "a small domesticated carnivorous mammal",
 }
 print(dictionary)
+
