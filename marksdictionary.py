2ca7b1f1b67221b7850ac6b003021b85df924b9c
Prajukta <prajuktaha@gmail.com>
2025-09-07 22:26:11 +0530

    wap marks and store them in dict

---
+++ marksdictionary.py
@@ -0,0 +1,15 @@
+marks= {}
+
+x=int(input("enter physics mark :"))
+marks.update({"physics" : x})
+
+x=int(input("enter maths mark :"))
+marks.update({"maths" : x})
+
+x=int(input("enter chem mark :"))
+marks.update({"chem" : x})
+
+x=int(input("enter english mark :"))
+marks.update({"english" : x})
+
+print(marks)
