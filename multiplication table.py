3d2e88c2457abba61d58766e38c0d180a6682c27
Prajukta <prajuktaha@gmail.com>
2025-09-09 02:14:17 +0530

    multiplication table

---
+++ multiplication table.py
@@ -0,0 +1,7 @@
+#print the multiplication table of a number
+num = int(input("Enter the number"))
+i = 1
+while i<=10:
+    print( num , "X" , i , "=" , num*i)
+    i+=1
+print("here is your multiplication table of" , num)     
