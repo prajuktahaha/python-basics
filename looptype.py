f96962e79d688854597849293d343e85445420d6
Prajukta <prajuktaha@gmail.com>
2025-09-14 22:35:23 +0530

    loop in list , tuple , string

---
+++ looptype.py
@@ -0,0 +1,19 @@
+#for loop in list
+list = [1 , 2 , 3  ,4 , 5]
+for val in list:
+    print(val)
+
+
+#for loop in tuple
+tup = (1 , 2 , 3 , 4 , 5)
+for a in tup:
+    print(a)
+
+#for loop in string using else
+str = "hi prajukta"
+for char in str:
+   #to search character value
+   if char == 'a':
+       print("character found")
+   print(char)
+  
