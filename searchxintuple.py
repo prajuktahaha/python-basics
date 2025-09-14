ab29082b93af2c534931004a174cb719938a8c00
Prajukta <prajuktaha@gmail.com>
2025-09-14 22:44:37 +0530

    search a number x inn the tuple using for loop

--- searchxintuple.py
+++ searchxintuple.py
@@ -1,9 +1,11 @@
 #search a number x inn the tuple using for loop
 tuple = (1 , 4 , 9 ,16 , 25 , 36 , 49 , 64 , 81 , 100)
 x = int(input("Enter the number to be searched:"))
+idx = 0
 for i in tuple:
     if(i == x):
-        print("bingo ! x found")
+        print("bingo ! x found at index", idx)
         break
+    idx += 1
 else:
     print("x not found")    
