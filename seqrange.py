269c11a1ea72ee54869dc5c37a848ecaa5321767
Prajukta <prajuktaha@gmail.com>
2025-09-15 23:51:33 +0530

    range for loop

--- seqrange.py
+++ seqrange.py
@@ -5,3 +5,8 @@ for i in range(5,10):
     print(i)    
 for i in range (1 , 10 , 4):
     print(i)    
+    #it will print 1 , 5 ,9 why? cuz here it incereses by 4
+    #here 1 is starting point , 10 is ending point and 4 is step
+for i in range (10 , 1 , -2):
+    print(i)   #it will print 10 , 8 , 6 , 4 , 2 why? cuz here it decreases by 2
+    #here 10 is starting point , 1 is ending point and -2 is step
